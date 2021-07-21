3d5df9b7bb8e3fba3d917a8aa6212b1987a1b49d
BA_CalderonMorales <tbcm640@gmail.com>
2021-07-21 13:31:23 -0500

    Created comment model inside of youtube_app.

--- youtube_project/youtube_app/models.py
+++ youtube_project/youtube_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Comment(models.Model):
+    videoId = models.CharField(max_length=25)
+    userComment = models.CharField(max_length=500)
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
